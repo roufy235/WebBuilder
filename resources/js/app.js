4bac540d5091a9ace975efc85681285256733d8e
roufy235 <roufy235@gmail.com>
2023-06-08 08:26:33 +0100

    feat: setup

---
+++ resources/js/app.js
@@ -0,0 +1,43 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+import './bootstrap';
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import ExampleComponent from './components/ExampleComponent.vue';
+import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+]
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+})
+
+const app = createApp({});
+
+app.component('example-component', ExampleComponent);
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
+//     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
+// });
+
+app.use(router)
+app.mount('#app');
---
+++ resources/js/views/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline italic">
+            Hello world!
+        </h1>
+        <router-link to="/about">
+            About
+        </router-link>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
